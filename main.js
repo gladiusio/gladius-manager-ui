d10bf2ee9f5c7c8f55cd620279d06148fc7b0ba3
Nathan Feldsien <nate@studioember.com>
2018-05-31 14:10:42 -0400

    Adds electron rework

--- main.js
+++ main.js
@@ -1,54 +1,87 @@
-var {
+let {
   app,
   BrowserWindow,
   Tray,
   Menu
 } = require('electron')
 
-var path = require('path')
-var url = require('url')
-var iconpath = path.join(__dirname, 'icon.png') // path of y
-var win
+let path = require('path')
+let url = require('url')
+let win
+
+// Icons
+let imageFolder = __dirname + '/img'
+let platform = require('os').platform()
+let iconPath = path.join(imageFolder, '/icon/icon.png')
+let trayImage
+
+// Determine appropriate icon for platform
+if (platform == 'darwin') {
+  trayImage = path.join(imageFolder, '/tray/mac/icon.png')
+  app.dock.hide()
+}
+else if (platform == 'win32') {
+  trayImage = path.join(imageFolder, '/tray/win/icon.ico')
+}
 
 function createWindow() {
   win = new BrowserWindow({
-    width: 600,
-    height: 600,
-    icon: iconpath
+    width: 800,
+    height: 800,
+    icon: iconPath
   })
 
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  var appIcon = new Tray(iconpath)
-
-  var contextMenu = Menu.buildFromTemplate([{
-      label: 'Show Gladius Manager',
-      click: function() {
-        win.show()
+  win.loadURL(
+    url.format(
+      {
+        pathname: path.join(__dirname, 'dist/index.html'),
+        protocol: 'file:',
+        slashes: true
       }
-    },
-    {
-      label: 'Quit',
-      click: function() {
-        app.isQuiting = true
-        app.quit()
-      }
-    }
-  ])
+    )
+  )
 
-  appIcon.setContextMenu(contextMenu)
+  if (platform != "darwin") {
+    let appIcon = new Tray(trayImage)
+    appIcon.setPressedImage(imageFolder + '/tray/mac/iconHighlight.png');
+  }
+
+  let contextMenu = Menu.buildFromTemplate(
+    [
+      {
+        label: 'Show Gladius Manager',
+        click: function() {
+          win.show()
+        }
+      },
+      {
+        label: 'Quit',
+        click: function() {
+          // gladius-controld stop
+          app.isQuiting = true
+          app.quit()
+        }
+      }
+    ]
+  )
+
+  if (platform != "darwin") {
+    appIcon.setContextMenu(contextMenu)
+  }
 
   win.on('close', function(event) {
-    if(!app.isQuiting){
-        event.preventDefault();
-        win.hide();
+    if (platform == "darwin") {
+      app.isQuiting = true
+      app.quit()
+      return true
     }
 
-    return false;
+    if(!app.isQuiting) {
+      event.preventDefault()
+      win.hide()
+    }
+
+    return false
   })
 
   win.on('minimize', function(event) {
@@ -57,9 +90,10 @@ function createWindow() {
   })
 
   win.on('show', function() {
-    appIcon.setHighlightMode('always')
+    if (platform != "darwin") {
+      appIcon.setHighlightMode('selection')
+    }
   })
-
 }
 
 app.on('ready', createWindow)
