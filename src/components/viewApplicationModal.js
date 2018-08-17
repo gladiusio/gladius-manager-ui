7fcaa0f8044e753c8f962be8dc599396903a33f8
Kenny Tran <kenny8tran@gmail.com>
2018-08-17 16:03:26 -0700

    Show pool name and address in modal

--- src/components/viewApplicationModal.js
+++ src/components/viewApplicationModal.js
@@ -9,6 +9,15 @@ import bemify from '../util/bemify';
 const bem = bemify('view-application-modal');
 
 export default class ViewApplicationModal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showingAddress: false,
+    };
+
+    this.handleShowAddress = this.handleShowAddress.bind(this);
+  }
   renderField(title, value, suffix = '') {
     if (value === null || value === undefined || value === '') {
       return null;
@@ -19,6 +28,41 @@ export default class ViewApplicationModal extends Component {
         <div>{title}</div>
         <div className="text-muted">{value}{suffix}</div>
       </div>
+    );
+  }
+
+  handleShowAddress(e) {
+    e.preventDefault();
+    this.setState({showingAddress: true});
+  }
+
+  renderHeader(application) {
+    let address = null;
+    if (this.state.showingAddress) {
+      address = (
+        <span className="text-muted">{application.pool.address}</span>
+      );
+    } else {
+      address = (
+        <a href="#" className={bem('show-address')} onClick={this.handleShowAddress}>
+          View pool address
+        </a>
+      );
+    }
+
+    return (
+      <section>
+        <div className={classnames(bem('header-container'), 'p-4')}>
+          <h1 className="mb-2">Application Information</h1>
+          <p className="mb-3 text-muted">
+            This is the info that you submitted to the pool:
+          </p>
+          <p className="mb-0 text-muted">
+            {application.pool.name}
+          </p>
+          {address}
+        </div>
+      </section>
     );
   }
 
@@ -35,14 +79,7 @@ export default class ViewApplicationModal extends Component {
     return (
       <Modal onClose={onClose}>
         <div className={bem('wrapper')}>
-          <section>
-            <div className={classnames(bem('header-container'), 'p-4')}>
-              <h1 className="mb-2">Application Information</h1>
-              <p className="mb-0 text-muted">
-                This is the info that you submitted to the pool.
-              </p>
-            </div>
-          </section>
+          {this.renderHeader(application)}
           <section className={classnames(bem('content'), 'p-4')}>
             {this.renderField('Name', name)}
             {this.renderField('Email', email)}
