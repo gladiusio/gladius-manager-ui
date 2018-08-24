64e08d8fccc27748113bb6cb4275f9e0500a49c9
Kenny Tran <kenny8tran@gmail.com>
2018-08-24 13:41:49 -0700

    Add sentry

--- src/index.js
+++ src/index.js
@@ -8,19 +8,35 @@ import { ConnectedRouter } from 'react-router-redux';
 import App from './components/app';
 import { storeFactory, saveState } from './state/store';
 
+if (window.Raven) {
+  window.Raven.config(
+    'https://587a7c212c904b70afe329b94819f8ba@sentry.io/1268531'
+  ).install();
+}
+
 const history = createHistory();
 const store = storeFactory(undefined, history);
 
 store.subscribe(() => saveState(store.getState()));
 
-ReactDOM.render(
-  (
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <App />
-      </ConnectedRouter>
-    </Provider>
+function initApp() {
+  ReactDOM.render(
+    (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <App />
+        </ConnectedRouter>
+      </Provider>
 
-  ),
-  document.getElementById('root'),
-);
+    ),
+    document.getElementById('root'),
+  );
+}
+
+if (window.Raven) {
+  window.Raven.context(function () {
+    initApp();
+  });
+} else {
+  initApp();
+}
